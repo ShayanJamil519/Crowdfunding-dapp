f48b719f0af100498dccb200baebbd1a40e7a04f
ShayanJamil519 <shayanjamil500@gmail.com>
2023-03-14 21:52:17 +0500

    Hero Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Home from "./views/Home";
 
 const App = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen relative">
       <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
