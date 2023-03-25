cded51dbdcd6f26826e157175bcc1a672fb3d5b7
ShayanJamil519 <shayanjamil500@gmail.com>
2023-03-25 22:08:36 +0500

    Hardhat.config.js Configured

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,17 @@
-require('@nomiclabs/hardhat-waffle')
-require('dotenv').config()
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
+require("dotenv").config();
+
+const PRIVATE_KEY1 = process.env.PRIVATE_KEY1;
+const PRIVATE_KEY2 = process.env.PRIVATE_KEY2;
+const PRIVATE_KEY3 = process.env.PRIVATE_KEY3;
+const PRIVATE_KEY4 = process.env.PRIVATE_KEY4;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 module.exports = {
-  defaultNetwork: 'localhost',
-  networks: {
-    localhost: {
-      url: 'http://127.0.0.1:8545',
-    },
-  },
   solidity: {
-    version: '0.8.11',
+    version: "0.8.7",
     settings: {
       optimizer: {
         enabled: true,
@@ -17,11 +19,56 @@ module.exports = {
       },
     },
   },
-  paths: {
-    sources: './contracts',
-    artifacts: './src/abis',
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    localhost: {
+      chainId: 31337,
+      url: "http://127.0.0.1:8545",
+    },
+
+    sepolia: {
+      chainId: 5,
+      blockConfirmations: 6,
+      url: SEPOLIA_RPC_URL,
+      accounts:
+        PRIVATE_KEY1 !== undefined &&
+        PRIVATE_KEY2 !== undefined &&
+        PRIVATE_KEY3 !== undefined &&
+        PRIVATE_KEY4 !== undefined
+          ? [PRIVATE_KEY1, PRIVATE_KEY2, PRIVATE_KEY3, PRIVATE_KEY4]
+          : [],
+    },
   },
+
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    noColors: false,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    address1: {
+      default: 1,
+    },
+    address2: {
+      default: 2,
+    },
+    address3: {
+      default: 3,
+    },
+  },
+  // paths: {
+  //   sources: "./contracts",
+  //   artifacts: "./src/abis",
+  // },
   mocha: {
     timeout: 40000,
   },
-}
+};
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,24 +1,35 @@
-const { ethers } = require('hardhat')
-const fs = require('fs')
+const { ethers } = require("hardhat");
+const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-  const contract_name = ''
-  const Contract = await ethers.getContractFactory(contract_name)
-  const contract = await Contract.deploy()
+  // Setting Accounts
+  [deployer, address1, address2, address3] = await ethers.getSigners();
+  const PROJECT_TAX = 10;
 
-  await contract.deployed()
+  // Deploying BoostMeUp Contract
+  const BoostMeUp = await ethers.getContractFactory("BoostMeUp");
+  const boostMeUp = await BoostMeUp.deploy(PROJECT_TAX);
+  await boostMeUp.deployed();
 
-  const address = JSON.stringify({ address: contract.address }, null, 4)
-  fs.writeFile('./src/abis/contractAddress.json', address, 'utf8', (err) => {
+  const address = JSON.stringify({ address: boostMeUp.address }, null, 4);
+  fs.writeFile("./src/abis/contractAddress.json", address, "utf8", (err) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
-    console.log('Deployed contract address', contract.address)
-  })
+    console.log(
+      "----------------------------------------------------------------------------"
+    );
+    console.log("Deployed BoostMeUp Contract at: ", boostMeUp.address);
+
+    console.log(
+      "----------------------------------------------------------------------------"
+    );
+  });
 }
 
 main().catch((error) => {
-  console.error(error)
-  process.exitCode = 1
-})
+  console.error(error);
+  process.exitCode = 1;
+});
